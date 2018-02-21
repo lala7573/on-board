40898defb79f5d04d5d004cff80cf1946fef383f
nauco <hodong42@gmail.com>
2018-02-21 19:51:31 +0900

    db->ejs
    some changes tables description
    apply ejs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var mysql = require('mysql');
 var path = require('path');
 var bodyParser = require('body-parser');
+var ejs = require('ejs');
 var app = express();
 var router = express.Router();
 var pool = mysql.createPool({
@@ -11,17 +12,27 @@ var pool = mysql.createPool({
   database: 'board'
 });
 
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
 
 router.get('/', function (req, res) {
-  res.sendFile(__dirname+'/index.html');
+  //res.sendFile(__dirname+'/index.html');
+  pool.getConnection(function(err, connection){
+    connection.query('SELECT * FROM posts', function(err, rows, fields){
+      if (err) throw err;
+      console.log(rows);
+      obj = {print: rows};
+      res.render('index', obj);
+  })
 });
+});
+
 router.get('/add', function (req, res) {
   res.sendFile(__dirname+'/add.html');
 });
 router.post('/add', function(req, res){
-
   var post = {'contents':req.body.contents};
   res.send('hi ' + post.contents);
   pool.getConnection(function(err, connection){
